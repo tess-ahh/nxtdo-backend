255d488693c20fee7f3636a4d834a997646c77c0
tess-ahh <tessamariya295@gmail.com>
2026-02-13 21:58:07 +0530

    added api-backend

--- app/main.py
+++ app/main.py
@@ -78,7 +78,9 @@ def create_task(task: TaskCreate):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-
+@app.get("/about",response_class=PlainTextResponse)
+def about_backend():
+    return "This is all backend"
 
 @app.get("/tasks")
 def list_tasks(limit: int = 100):
